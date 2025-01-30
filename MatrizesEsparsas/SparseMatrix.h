ec3fb9e8a5b34f6c7a3e7f6c558b649ba57e189f
Grazielly <grazielly2005lima@gmail.com>
2025-01-30 15:06:29 -0300

    Co-authored-by: ThalissonPR <ThalissonPR@users.noreply.github.com>

--- MatrizesEsparsas/SparseMatrix.h
+++ MatrizesEsparsas/SparseMatrix.h
@@ -5,29 +5,52 @@
 #include <stdexcept>
 #include "Node.h"
 
+using namespace std;
+
 class SparseMatrix {
-    
 private:
 
     int linhas, colunas;
     Node* m_headLinha;
     Node* m_headColuna;
+    
 
 public:
 
 // OBS: Grazi, esse comentários são apenas para nós 2
 // Não é pro Atílio, não se preocupe ;)
 
-    // Construtor
-    SparseMatrix(int m, int n) {
+    // Construtor que recebe como parâmetro o numero de linhas e colunas
+    void SparseMatrix(int m, int n) {
         if (m <= 0 || n <= 0) {
             throw invalid_argument("As medidas precisam ser positivas");
+            return;
         }
-
+            
+        linhas = m;
+        colunas = n;
+            
         m_headLinha = new Node(0, 0, 0);
-        m_headColuna = new Node(0, 0, 0);
+        Node* auxLinha = m_headLinha;
 
-        // Continuo depois...
+        for(int i = 1; i <= m; i++) {
+            Node* node = new Node(i, 0, 0);
+            auxLinha -> abaixo =  node;
+            auxLinha = node;
+            auxLinha -> direita = auxLinha;
+        }
+        auxLinha->abaixo = m_headLinha;
+
+        m_headColuna = new Node(0, 0, 0);
+        Node* auxColuna = m_headColuna;
+        
+        for(int i = 1; i <= n; i++) {
+            Node* node = new Node(0, i, 0);
+            auxColuna -> direita = node;
+            auxColuna = node;
+            auxColuna -> abaixo = auxColuna;
+        }
+        auxColuna->direita = m_headColuna;
     }
 
     // Destrutor
@@ -48,7 +71,18 @@ public:
     }
 
     // Esse aqui pega os valores que estão na matriz
-    double get(int i, int j);
+    double get(int i, int j) {
+        Node* aux = new Node(0, 0, 0);
+        aux = m_headLinha;
+        int indice = 1;
+        for(int i = 1; i <= linhas; i++) {
+            for(int j = 1; j <= colunas; j++){
+
+            }
+        }
+
+
+    }
 
 
     // Esse só imprime a matriz pegando os valores pelo get
--- MatrizesEsparsas/main.cpp
+++ MatrizesEsparsas/main.cpp
@@ -4,10 +4,30 @@
 #include "SparseMatrix.h"
 using namespace std;
 
-void readSparseMatrix(SparseMatrix& m, String ?); //Temos que colocar o nome do arquivo como parâmetro
+void readSparseMatrix(SparseMatrix& m, string teste) {
+    ifstream arquivo(teste);
+    if (!arquivo) {
+        cerr << "Erro ao abrir o arquivo!\n";
+        return;
+    }
+
+    int linhas, colunas;
+    arquivo >> linhas >> colunas; // Lendo número de linhas e colunas
+    m = SparseMatrix(linhas, colunas); // Ajustando o tamanho da matriz esparsa
+
+    int i, j;
+    double valor;
+    while (arquivo >> i >> j >> valor) { // Lendo cada tripla (linha, coluna, valor)
+        m.insert(i, j, valor); // Inserindo na estrutura de dados da matriz esparsa
+    }
+
+    arquivo.close();
+}
 
 SparseMatrix sum(SparseMatrix& A, SparseMatrix& B);
 
 SparseMatrix multiply(SparseMatrix& A, SparseMatrix& B);
 
 int main();
+
+
